feb4dd144ed7a4da9041723fc7f87d88268718ff
Tran Duc Quy <tranducquylhp@gmail.com>
2019-11-11 11:27:01 +0700

    ok

--- src/main/java/com/codegym/demo_chatbot_fb/controller/WebhookRestController.java
+++ src/main/java/com/codegym/demo_chatbot_fb/controller/WebhookRestController.java
@@ -1,58 +1,25 @@
 package com.codegym.demo_chatbot_fb.controller;
 
 import com.github.messenger4j.Messenger;
-import com.github.messenger4j.common.WebviewHeightRatio;
 import com.github.messenger4j.exception.MessengerApiException;
 import com.github.messenger4j.exception.MessengerIOException;
 import com.github.messenger4j.exception.MessengerVerificationException;
 import com.github.messenger4j.send.MessagePayload;
 import com.github.messenger4j.send.MessagingType;
 import com.github.messenger4j.send.NotificationType;
-import com.github.messenger4j.send.SenderActionPayload;
-import com.github.messenger4j.send.message.RichMediaMessage;
-import com.github.messenger4j.send.message.TemplateMessage;
 import com.github.messenger4j.send.message.TextMessage;
-import com.github.messenger4j.send.message.quickreply.LocationQuickReply;
-import com.github.messenger4j.send.message.quickreply.QuickReply;
-import com.github.messenger4j.send.message.quickreply.TextQuickReply;
-import com.github.messenger4j.send.message.richmedia.UrlRichMediaAsset;
-import com.github.messenger4j.send.message.template.ButtonTemplate;
-import com.github.messenger4j.send.message.template.GenericTemplate;
-import com.github.messenger4j.send.message.template.ListTemplate;
-import com.github.messenger4j.send.message.template.ReceiptTemplate;
-import com.github.messenger4j.send.message.template.button.*;
-import com.github.messenger4j.send.message.template.common.Element;
-import com.github.messenger4j.send.message.template.receipt.Address;
-import com.github.messenger4j.send.message.template.receipt.Adjustment;
-import com.github.messenger4j.send.message.template.receipt.Item;
-import com.github.messenger4j.send.message.template.receipt.Summary;
 import com.github.messenger4j.send.recipient.IdRecipient;
-import com.github.messenger4j.send.senderaction.SenderAction;
-import com.github.messenger4j.userprofile.UserProfile;
-import com.github.messenger4j.webhook.Event;
 import com.github.messenger4j.webhook.event.*;
-import com.github.messenger4j.webhook.event.attachment.Attachment;
-import com.github.messenger4j.webhook.event.attachment.LocationAttachment;
-import com.github.messenger4j.webhook.event.attachment.RichMediaAttachment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Instant;
-import java.util.*;
 
 import static com.github.messenger4j.Messenger.*;
-import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.*;
-import static com.github.messenger4j.send.message.richmedia.RichMediaAsset.Type.FILE;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 
@@ -74,14 +41,6 @@ public class WebhookRestController {
                                                 @RequestParam("hub.verify_token") final String verifyToken, */
                                                 @RequestParam("hub.challenge") final String challenge) {
         return ResponseEntity.ok(challenge);
-//        logger.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
-//        try {
-//            this.messenger.verifyWebhook(mode, verifyToken);
-//            return ResponseEntity.ok(challenge);
-//        } catch (MessengerVerificationException e) {
-//            logger.warn("Webhook verification failed: {}", e.getMessage());
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-//        }
     }
 
     @PostMapping("/webhook")
@@ -112,23 +71,10 @@ public class WebhookRestController {
         final Instant timestamp = event.timestamp();
 
         logger.info("Received message '{}' with text '{}' from user '{}' at '{}'", messageId, messageText, senderId, timestamp);
-        final IdRecipient recipient = IdRecipient.create(senderId);
-        final NotificationType notificationType = NotificationType.REGULAR;
-        final String metadata = "DEVELOPER_DEFINED_METADATA";
-
-        logger.info("abc1");
-
-        final TextMessage textMessage = TextMessage.create("Hello", empty(), of(metadata));
-
-        logger.info("abc2");
-        final MessagePayload messagePayload = MessagePayload.create(recipient, MessagingType.RESPONSE, textMessage);
-
-        logger.info(messagePayload.toString());
-        this.messenger.send(messagePayload);
-        logger.info("abc4");
-        //sendTextMessage(senderId, "Hello");
+        sendTextMessage(senderId, "Hello");
     }
 
+    @Scheduled(cron = "0 30 11 * * MON-THU")
     private void sendTextMessage(String recipientId, String text) {
         try {
             final IdRecipient recipient = IdRecipient.create(recipientId);
