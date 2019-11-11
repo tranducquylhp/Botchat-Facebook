259c847ca9896dbcd37bcd3ad8e25e74b707ddbd
Tran Duc Quy <tranducquylhp@gmail.com>
2019-11-11 09:42:45 +0700

    cm

--- src/main/java/com/codegym/demo_chatbot_fb/controller/WebhookRestController.java
+++ src/main/java/com/codegym/demo_chatbot_fb/controller/WebhookRestController.java
@@ -1,44 +1,48 @@
 package com.codegym.demo_chatbot_fb.controller;
 
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.exception.MessengerVerificationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import static com.github.messenger4j.Messenger.*;
+
 @RestController
 public class WebhookRestController {
-    @Value("${verifyToken}")
-    String VERYFY_TOKEN;
+    private static final String RESOURCE_URL = "https://raw.githubusercontent.com/fbsamples/messenger-platform-samples/master/node/public";
+
+    private static final Logger logger = LoggerFactory.getLogger(WebhookRestController.class);
+
+    private final Messenger messenger;
+
+    @Autowired
+    public WebhookRestController(final Messenger messenger) {
+        this.messenger = messenger;
+    }
+
 
 
     @GetMapping("/webhook")
-    public ResponseEntity<String> veryfyWebhook(HttpServletRequest request, HttpServletResponse response) {
-        String mode = request.getParameter("hub.mode");
-        String token = request.getParameter("hub.verify_token");
-        String challenge = request.getParameter("hub.challenge");
-
-
-        // Checks the mode and token sent is correct
-        if (mode != null && token != null) {
-            if (mode == "subscribe" && token == VERYFY_TOKEN) {
-
-                // Responds with the challenge token from the request
-                return new ResponseEntity<String>(challenge, HttpStatus.OK);
-
-            } else {
-                // Responds with '403 Forbidden' if verify tokens do not match
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+                                                @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken, @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
+        logger.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode, verifyToken, challenge);
+        try {
+            this.messenger.verifyWebhook(mode, verifyToken);
+            return ResponseEntity.ok(challenge);
+        } catch (MessengerVerificationException e) {
+            logger.warn("Webhook verification failed: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
         }
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/webhook")
